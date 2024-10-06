f94fa337905a396d1d9946bd847773f609b97f50
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2024-10-06 17:40:24 +0300

    cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'http://localhost:5173'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
