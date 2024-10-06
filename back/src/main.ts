b292093f0edaff1c0b09006f84e71a1531ba2a29
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2024-10-06 18:03:01 +0300

    third

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,7 +5,11 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'https://react-menucheck.onrender.com/',
+    ],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
